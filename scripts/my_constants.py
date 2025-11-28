aad769fb8949e4cd2c448068e04dd123b52caf6b
Valérian Grégoire--Bégranger <valerian.greg@gmail.com>
2025-11-28 15:17:33 +0100

    Forgot to add my constants

--- scripts/my_constants.py
+++ scripts/my_constants.py
@@ -40,8 +40,8 @@ BOX_TYPE = 1
 OBSTACLE_TYPE = 2
 
 """ GUI """
-BG_COLOR = (190, 250, 100)
-WHITE = (239, 255, 160)
-BLACK = (0, 0, 0)
+BG_COLOR = (0, 0, 0)
+WHITE = (0, 0, 0)
+BLACK = (255, 0, 120)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
